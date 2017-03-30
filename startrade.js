3bfd280b2574d4d54f63caa1f8f3b82003ae79af
John Goodman <johncurtisgoodman@gmail.com>
2017-03-29 23:04:36 -0400

    Set up menu label animation

--- startrade.js
+++ startrade.js
@@ -1,10 +1,49 @@
+//STARTRADE
+
+//On window load...
 onload = function () {
   var game = new GameManager ();
   game.start();
   game.describePlanet(game.player);
+  menuSetup();
 };
 
-onkeydown = function () {
+// cssEffects.js
+document.flash = function (el) {
+  var currentColor = '#0ac'
+  el.style.color = '#fff'
+  el.style.transition = 'color .1s'
+  setTimeout(function () {
+    el.style.transition = ''
+    el.style.color = currentColor;
+  }, 150)
+}
+
+// MenuSetup.js
+var menuSetup = function () {
+  var selector = document.getElementById('menu-selector');
+
+  var status = document.getElementById('status');
+  var controls = document.getElementById('controls');
+  var cargo = document.getElementById('cargo');
+  var action = document.getElementById('action');
+  var map = document.getElementById('map');
+  var menu = [status, controls, cargo, action, map];
+
+  menu.forEach(function (item) {
+    item.onmouseover = function () {
+      selector.innerText = this.id;
+      document.flash(selector);
+    }
+    item.onmouseleave = function () {
+      selector.innerText = '';
+      document.flash(selector);
+    }
+  });
+};
+
+// KeyControls.js
+onkeydown = function (event) {
   if (event.keyCode == '69') { // "e" key
     var container = document.getElementsByTagName('container')[0];
     if (container.mozRequestFullScreen) {
@@ -15,6 +54,7 @@ onkeydown = function () {
   }
 };
 
+// Planet.js
 var Planet = function (name) {
   this.name = name;
   this.location = {
@@ -27,6 +67,7 @@ var Planet = function (name) {
   this.commodities = [];
 };
 
+// Commodities.js
 var Commodity = function (name) {
   this.name = name;
 };
@@ -36,10 +77,12 @@ var CommodityStock = function (commodity, amount) {
   this.amount = amount;
 };
 
+// Player.js
 var Player = function (planet) {
   this.planet = planet;
 };
 
+// Gamemanager.js
 var GameManager = function () {};
 
 GameManager.prototype.start = function () {
